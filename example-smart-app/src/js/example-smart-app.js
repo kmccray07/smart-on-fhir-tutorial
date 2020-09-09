da5b78d5630cff96995df4079c6e01013f5040a9
kmccray07 <39418849+kmccray07@users.noreply.github.com>
2020-09-09 12:13:30 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -68,10 +68,7 @@
     }
 
     FHIR.oauth2.settings.fullSessionStorageSupport = false;
-    //FHIR.oauth2.ready(onReady, onError);
-    FHIR.oauth2.ready()
-    .then(onReady)
-    .catch(onError);
+    FHIR.oauth2.ready(onReady, onError);
     
     return ret.promise();
 
